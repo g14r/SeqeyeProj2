8879482eeddbd6b9fa3605ba1156d9209c26c9e7
Neda Kordjazi <NKordjaz@uwo.ca>
2017-12-06 20:45:11 -0500

    from macbook to repo

---
+++ se2_Crossval_GLM_ridge.m
@@ -0,0 +1,130 @@
+function out  = se2_Crossval_GLM_ridge(what , lambda)
+
+
+% what  'c' for modeling Chunked sequences 
+%       'r' for modeling Random sequences 
+
+
+% baseDir = '/Users/nedakordjazi/Documents/SeqEye/SeqEye2/analyze';     %macbook
+baseDir = '/Volumes/MotorControl/data/SeqEye2/analyze';  % server
+subj_name = {'AT1' , 'CG1' , 'HB1' , 'JT1' , 'CB1' , 'YM1' , 'NL1' , 'SR1' , 'IB1' , 'MZ1' , 'DW1', 'All'};
+%% develope the GLM
+horzSize = {1,2,3,4,5,6,7,8,13,[5:13]}; 
+switch what
+    case 'c'
+        load([baseDir , '/se2_TranProb.mat'] , 'C')
+        M = C;
+        titleSuffix = 'Chunked';
+        
+    case 'r'
+        load([baseDir , '/se2_TranProb.mat'] , 'R')
+        M = R;
+        titleSuffix = 'Random';
+end
+% =================== Prepare the Design Matrix
+L = length(M.IPI);
+M.IPIarrangement(M.IPIarrangement == 0) = 1;
+M.X1 = ones(L , 1); % intercept
+
+M.X2 = M.IPIarrangement; % within/between chunk
+M.X2(M.X2==1) = 1; % between
+M.X2(M.X2==0) = 1; % Random --> set to between
+M.X2(M.X2==2) = -1;  % within
+
+% Map 1st, 2nd 3rd transition probabilities to 5 bins and set them as predictors
+temp = M.t2Prob_n;
+M.X3 = ceil(1.5*(1 + mapminmax(temp')))';
+
+temp = M.t3Prob_n(:,1);
+M.X4 = ceil(1.5*(1 + mapminmax(temp')))';
+
+temp = M.t4Prob_n(:,1);
+M.X5 = ceil(1.5*(1 + mapminmax(temp')))';
+
+% the learinng regressor - using normalized IPIs, we dont need this
+M.X6 = M.BN;
+
+% the repetition regressor
+M.X7 = ismember(M.t2 , [21:25])  +1;
+
+% the design mtrix
+M.X = [M.X1 M.X2 M.X3 M.X4 M.X5 M.X6 M.X7];
+
+
+M.Y = M.IPI_norm;
+titleSuffix = [titleSuffix , '-norm'];
+xx =     {[7]    [2 7] ,   [3 7] ,   [4 7]        [3:5,7]          ,[2:3  , 7] ,      [2:4,7]  ,    [2:5 , 7]};
+label = {'R'  'C+R',     '1st+R' ,'1st+2nd+R'    '1st+2nd+3rd+R' , 'C+1st+R'  ,  'C+1st+2nd+R'  ,  'Full'};
+plotIND = [2:5 , 8]; % the model indices to include in plot
+
+cleanLabel = {'within/between Chunk', '1st order probability' ,'1st + 2nd order probability' ,'1st + 2nd + 3rd order probability' ,'Full Model'};
+cat_cleanLabel = categorical(repmat(cleanLabel , length(horzSize)-1 , 1));
+% =================== % =================== % =================== %
+% =================== Make Modelss
+% 3 fold crossvalidation
+CVfol = 3;
+count = 1;
+for h = 1:length(horzSize) % loop over horizons
+    for dd = 1:5   % loop over days
+        for sn = 1:length(subj_name) - 1   % loop over subjects
+            T = getrow(M , ismember(M.SN , sn) & ismember(M.Horizon , horzSize{h}) & ismember(M.Day , dd));
+            L = length(T.Y);
+            CVI = crossvalind('Kfold', L, CVfol);
+            for cvl = 1:CVfol  % loop over CV folds
+                Test = getrow(T , CVI==cvl);
+                Train = getrow(T , CVI~=cvl);
+                params  = xx{1};   % Null Model
+                X = Train.X(:,params);
+                X = X - repmat(mean(X) , length(X) , 1); % mean subtract the design matrix
+                Y = Train.Y - mean(Train.Y);  % mean subtract the output
+
+                % Matlab 2016 onward (iMac)
+                % Mdl = fitrlinear(X ,Y,'Lambda',lambda,'Regularization','ridge' , 'FitBias' , false); Matlab 2016 onward
+                
+                % Matlab 2015 before (mcBook)
+                b = ridge(Y,X,lambda);
+
+                
+                X = Test.X(:,params);
+                X = X - repmat(mean(X) , length(X) , 1); % mean subtract the design matrix
+                Ypred0 = X * b; % prediction of the null model
+                for ml = 1:length(xx)
+                    params  = xx{ml};
+                    X = Train.X(:,params);
+                    X = X - repmat(mean(X) , length(X) , 1); % mean subtract the design matrix
+                    Y = Train.Y - mean(Train.Y);  % mean subtract the output
+                    
+                    % Matlab 2016 onward (iMac)
+                    % Mdl = fitrlinear(X ,Y,'Lambda',lambda,'Regularization','ridge' , 'FitBias' , false); Matlab 2016 onward
+                    
+                    % Matlab 2015 before (mcBook)
+                    b = ridge(Y,X,lambda);
+                    
+                    X = Test.X(:,params);
+                    X = X - repmat(mean(X) , length(X) , 1); % mean subtract the design matrix
+                    Y = Test.Y - mean(Test.Y);  % mean subtract the output
+                    
+                    Ypred = X * b;  % prediction of the alternative model
+                    
+                    out.R2(count,:)   = se2_R2ModelComp(Y , Ypred0 , Ypred);
+                    
+                    out.hor(count , 1) = h;
+                    out.subj(count , 1) = sn;
+                    temp = corrcoef(Y , Ypred);
+                    out.corYY (count , 1) = temp(2);
+                    out.day(count , 1) = dd;
+                    out.xx(count , 1) = ml;
+                    out.cv (count , 1) = cvl;
+                    count = count+1;
+                    disp(['Model ' , num2str(ml) , ' - Day ' , num2str(dd) , ' - Subject ' , num2str(sn) , ' - Horizon ' , num2str(h)])
+                end
+            end
+        end
+    end
+end
+Mdl = out;
+save([baseDir , '/se2_CrossvalIPI_',titleSuffix,'_RR.mat'] , 'Mdl' , '-v7.3')
+
+
+    
+ 
---
+++ se2_vizModelz.m
@@ -0,0 +1,186 @@
+function se2_vizModelz(what)
+
+% what  is 's' for shadeplot and 'b' for barplot
+
+
+
+% baseDir = '/Users/nedakordjazi/Documents/SeqEye/SeqEye2/analyze';     %macbook
+baseDir = '/Volumes/MotorControl/data/SeqEye2/analyze';  % server
+
+
+iN1 = input('Visualize Chunked or Random? (c/r)'  , 's');
+iN2 = input('Ridge or OLS? (r/o)' , 's');
+colors = [0 0 1;...
+    0 1 0;...
+    1 0 0;...
+    0 1 1;...
+    1 0 1;...
+    1 0.69 0.39;...
+    0.6 0.2 0;...
+    0 0.75 0.75;...
+    0.22 0.44 0.34;...
+    0.32 0.19 0.19];
+
+%% load proper model
+legenddHor = {'H = 1', 'H = 2','H = 3','H = 4','H = 5','H = 6','H = 7','H = 8','H = 13' , 'H = 5-13'};
+switch iN1
+    case 'c'
+        titleSuffix = 'Chunked';
+        switch iN2
+            case 'o'
+                load([baseDir , '/se2_CrossvalIPI_',titleSuffix,'-norm_OLS.mat'])
+            case 'r'
+                load([baseDir , '/se2_CrossvalIPI_',titleSuffix,'-norm_RR.mat']) 
+        end
+        
+        
+    case 'r'
+        titleSuffix = 'Random';
+        switch iN2
+            case 'o'
+                load([baseDir , '/se2_CrossvalIPI_',titleSuffix,'-norm_OLS.mat'])
+            case 'r'
+                load([baseDir , '/se2_CrossvalIPI_',titleSuffix,'-norm_RR.mat']) 
+        end
+        
+end
+%% set up legends and labels
+horzSize = {1,2,3,4,5,6,7,8,13,[5:13]}; 
+% x labels for shadeplot
+label = {'R'  'C+R',     '1st+R' ,'1st+2nd+R'    '1st+2nd+3rd+R' , 'C+1st+R'  ,  'C+1st+2nd+R'  ,  'Full'};
+plotIND = [2:5 , 8]; % the model indices to include in bar plot
+
+% Model descriptions
+cleanLabel = {'within/between Chunk', '1st order probability' ,'1st + 2nd order probability' ,'1st + 2nd + 3rd order probability' ,'Full Model'};
+cat_cleanLabel = categorical(repmat(cleanLabel , length(horzSize)-1 , 1));
+
+% =================== % =================== % =================== % =================== summarize
+dayz = {[1] [2 3] [4 5]};
+K = tapply(Mdl , {'hor' , 'subj' , 'day' , 'xx'} , {'R2' , 'nanmean'} , {'corYY' , 'nanmean'} ); % average over cv loops
+
+clear xp_dev pp_dev ep_dev xp_r2 pp_r2 ep_r2 xp_r2a pp_r2a ep_r2a dev_image R2_image xp_dn  pp_dn ep_dn dvn_image xp_cor  pp_cor  ep_cor
+h1 = figure;
+% I find color setting and legending  in lineplot a pain, so I generate the data using lineplot, close that figure and pop them into shadeplot and barplot
+for h = 1:length(horzSize)
+    for dd = 1:length(dayz)
+        [xp_cor{dd}(h, :) , pp_cor{dd}(h,:) , ep_cor{dd}(h,:)]  = lineplot(K.xx, K.corYY , 'plotfcn','nanmean' ,  'subset' , K.hor == h & ismember(K.day , dayz{dd}));
+        hold on
+        [xp_r2{dd}(h, :) , pp_r2{dd}(h,:) , ep_r2{dd}(h,:)]  = lineplot(K.xx, K.R2 , 'plotfcn','nanmean' , 'subset' , K.hor == h & ismember(K.day , dayz{dd}));
+        hold on
+    end
+end
+close(h1)
+
+
+%% do the plotting
+switch what
+    case 's'
+        % =================== % =================== % =================== % =================== Visualize model R2 comparisons
+        figure('color' , 'white')
+        
+        for dd = 1:length(dayz)
+            subplot(2,3,dd)
+            for h = 1:length(horzSize)-1
+                hold on
+                eval(['h' , num2str(h) , ' = plotshade([1:length(plotIND)] , pp_r2{dd}(h,plotIND) , ep_r2{dd}(h,plotIND),''transp'' , .2 , ''patchcolor'' , colors(h,:) , ''linecolor'' , colors(h,:) , ''linewidth'' , 3 , ''linestyle'' , '':'')']);
+                hold on
+            end
+            ylabel('R^2')
+            set(gca ,'YLim' , ylim, 'XLim' , [0 length(plotIND)+1] ,'XTick' , [1: length(plotIND)] , 'XTickLabels' , cleanLabel , 'FontSize' , 20 ,...
+                'XTickLabelRotation',45,'Box' , 'off' , 'GridAlpha' , 1)
+            title([titleSuffix , ' R^2, Days ' , num2str(dayz{dd})])
+            
+            grid on
+        end
+        legend([h1,h2,h3,h4,h5,h6,h7,h8,h9] ,legenddHor(1:end-1), 'Box' , 'off')
+        
+        
+        for dd = 1:length(dayz)
+            subplot(2,3,dd+length(dayz))
+            for h = length(horzSize)
+                hold on
+                eval(['h' , num2str(h) , ' = plotshade([1:length(plotIND)] , pp_r2{dd}(h,plotIND) , ep_r2{dd}(h,plotIND),''transp'' , .2 , ''patchcolor'' , colors(h,:) , ''linecolor'' , colors(h,:) , ''linewidth'' , 3 , ''linestyle'' , '':'')']);
+                hold on
+            end
+            ylabel('R^2')
+            set(gca ,'YLim' , ylim, 'XLim' , [0 length(plotIND)+1],'XTick' , [1: length(plotIND)] , 'XTickLabels' , cleanLabel , 'FontSize' , 20 ,...
+                'XTickLabelRotation',45,'Box' , 'off' , 'GridAlpha' , 1)
+            title([titleSuffix , ' R^2, Days ' , num2str(dayz{dd})])
+            grid on
+        end
+        legend([h10] ,legenddHor(end), 'Box' , 'off')
+        
+        % =================== % =================== % =================== % =================== Visualize model correlation comparisons
+        
+        figure('color' , 'white')
+        
+        for dd = 1:length(dayz)
+            subplot(2,3,dd)
+            for h = 1:length(horzSize)-1
+                hold on
+                eval(['h' , num2str(h) , ' = plotshade([1:length(plotIND)] , pp_cor{dd}(h,plotIND) , ep_cor{dd}(h,plotIND),''transp'' , .2 , ''patchcolor'' , colors(h,:) , ''linecolor'' , colors(h,:) , ''linewidth'' , 3 , ''linestyle'' , '':'')']);
+                hold on
+            end
+            ylabel('correlation')
+            set(gca , 'XLim' , [1 length(plotIND)+1],'XTick' , [1: length(plotIND)] , 'XTickLabels' , label(1:end) , 'FontSize' , 20 ,...
+                'XTickLabelRotation',45,'Box' , 'off' , 'GridAlpha' , 1)
+            title([titleSuffix , ' Model Correlations, Days ' , num2str(dayz{dd})])
+            
+            grid on
+        end
+        legend([h1,h2,h3,h4,h5,h6,h7,h8,h9] ,legenddHor(1:end-1), 'Box' , 'off')
+        
+        
+        for dd = 1:length(dayz)
+            subplot(2,3,dd+length(dayz))
+            for h = length(horzSize)
+                hold on
+                eval(['h' , num2str(h) , ' = plotshade([1:length(plotIND)] , pp_cor{dd}(h,plotIND) , ep_cor{dd}(h,plotIND),''transp'' , .2 , ''patchcolor'' , colors(h,:) , ''linecolor'' , colors(h,:) , ''linewidth'' , 3 , ''linestyle'' , '':'')']);
+                hold on
+            end
+            ylabel('correlation')
+            set(gca , 'XLim' , [1 length(plotIND)+1],'XTick' , [1: length(plotIND)] , 'XTickLabels' , label(1:end) , 'FontSize' , 20 ,...
+                'XTickLabelRotation',45,'Box' , 'off' , 'GridAlpha' , 1)
+            title([titleSuffix , ' Model Correlations, Days ' , num2str(dayz{dd})])
+            grid on
+        end
+        legend([h10] ,legenddHor(end), 'Box' , 'off')
+        
+        
+        
+    case 'b'
+        % *******************barplot
+        % =================== % =================== % =================== % =================== Visualize model R2 comparisons
+        xp_ = reshape(cell2mat(xp_r2) , size(xp_r2{1} , 1) , size(xp_r2{1} , 2) , length(xp_r2));
+        pp_ = reshape(cell2mat(pp_r2) , size(pp_r2{1} , 1) , size(pp_r2{1} , 2) , length(pp_r2));
+        ep_ = reshape(cell2mat(ep_r2) , size(ep_r2{1} , 1) , size(ep_r2{1} , 2) , length(ep_r2));
+        
+
+        figure('color' , 'white')
+        for i = 1:length(dayz)
+            subplot(length(dayz) ,1, i)
+            bar(squeeze(pp_(1:9,plotIND , i)));
+            grid on
+            set(gca , 'FontSize' , 20 ,'Box' , 'off' , 'GridAlpha' , 1 , 'XTick' , [1:length(legenddHor)-1] , 'XTickLabel' , legenddHor(1:end-1),'YLim' , ylim)
+            title([titleSuffix ,' - Model Crossvalidated R^2 on Day(s) ' , num2str(dayz{i})])
+        end
+        legend(cleanLabel)
+        % =================== % =================== % =================== % =================== Visualize model correlation comparisons
+        xp_ = reshape(cell2mat(xp_cor) , size(xp_cor{1} , 1) , size(xp_cor{1} , 2) , length(xp_cor));
+        pp_ = reshape(cell2mat(pp_cor) , size(pp_cor{1} , 1) , size(pp_cor{1} , 2) , length(pp_cor));
+        ep_ = reshape(cell2mat(ep_cor) , size(ep_cor{1} , 1) , size(ep_cor{1} , 2) , length(ep_cor));
+        
+        
+        figure('color' , 'white')
+        for i = 1:length(dayz)
+            subplot(length(dayz) ,1, i)
+            bar(squeeze(pp_(1:9,plotIND , i)));
+            grid on
+            set(gca , 'FontSize' , 20 ,'Box' , 'off' , 'GridAlpha' , 1 , 'XTick' , [1:length(legenddHor)-1] , 'XTickLabel' , legenddHor(1:end-1),...
+                'YLim' , [-.1 .4])
+            title([titleSuffix , ' - Model Prediction - Output Correlation on Day(s) ' , num2str(dayz{i})])
+        end
+        legend(cleanLabel)
+end
+
+
