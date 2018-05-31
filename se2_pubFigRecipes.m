4974584c5af6c8f2149f1f64395d04b55002a6a6
Nedak <nkordjaz@uwo.ca>
2018-05-31 17:59:48 -0400

    imactorepo

--- se2_pubFigRecipes.m
+++ se2_pubFigRecipes.m
@@ -1,11 +1,11 @@
 %% plotting recipes
 
-out  = se2_pubFigs(Dall , 'MT','RandvsStructCommpare');
-out  = se2_pubFigs(Dall , 'MT','RandStructAcrossDays' , 'poolDays' , 1);
-out  = se2_pubFigs(Dall , 'MT','compareLearning' , 'poolDays' , 0);
+out  = se2_pubFigs(Dall , 'MT','RandvsStructCommpare' , 'subjnum' , [1:15]);
+out  = se2_pubFigs(Dall , 'MT','RandStructAcrossDays' , 'poolDays' , 0 , 'subjnum' , [1:15]);
+out  = se2_pubFigs(Dall , 'MT','compareLearning' , 'poolDays' , 0 , 'subjnum' , [1:13]);
 out  = se2_pubFigs(Dall , 'MT','LearningEffectShade' , 'poolDays' , 0);
 out  = se2_pubFigs(Dall , 'MT','BoxFirstLastDays' , 'poolDays' , 0);
-out  = se2_pubFigs(Dall , 'MT','subjEffectiveHorizon' , 'poolDays' , 1);
+out  = se2_pubFigs(Dall , 'MT','subjEffectiveHorizon' , 'poolDays' , 1, 'subjnum' , [1:15]);
 
 
 Dall = Dall2;
@@ -24,7 +24,7 @@ out  = se2_pubFigs(Dall , 'RT','BoxFirstLastDays' , 'poolDays' , 0);
 
 out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fitHorz', 'poolDays' , 0, 'MaxIter' , 150);
 out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fitDayz', 'poolDays' , 0, 'MaxIter' , 50);
-out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fit%ChangeDayzTotalLearning', 'poolDays' , 1, 'MaxIter' , 50);
+out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fit%ChangeDayzTotalLearning', 'poolDays' , 0, 'MaxIter' , 50 , 'subjnum' , [1:15]);
 out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fit%ChangeDay2Day', 'poolDays' , 0, 'MaxIter' , 50);
 out  = se2_pubFigs(Dall , 'MT_asymptote','Actual&fit%ChangeSeqType', 'poolDays' , 0, 'MaxIter' , 50);
 out  = se2_pubFigs(Dall , 'MT_asymptote','plotCoef', 'poolDays' , 0, 'MaxIter' , 150);
@@ -33,8 +33,7 @@ out  = se2_pubFigs(Dall , 'MT_asymptote','', 'poolDays' , 0, 'MaxIter' , 150);
 out  = se2_pubFigs(Dall , 'IPI_asymptote','Actual&fitHorz', 'poolDays' , 0, 'MaxIter' , 300);
 out  = se2_pubFigs(Dall , 'IPI_asymptote','plotCoef', 'poolDays' , 0, 'MaxIter' , 200);
 out  = se2_pubFigs(Dall , 'IPI_asymptote','Actual&fit%ChangeDay2Day', 'poolDays' , 0, 'MaxIter' , 300);
-out  = se2_pubFigs(Dall , 'IPI_asymptote','Actual&fit%ChangeDayzTotalLearning', 'poolDays' , 0, 'MaxIter' , 300);
-
+out  = se2_pubFigs(Dall , 'IPI_asymptote','Actual&fit%ChangeDayzTotalLearning', 'poolDays' , 1, 'MaxIter' , 30);
 
 
 
@@ -85,9 +84,9 @@ lineplot(B.Day , B.MT , 'split' , B.exp , 'leg' , 'auto')
 
 
 %% significance test on MTs
-stats = se2_SigTest(Dall , 'MT' , 'seqNumb' , [0] , 'Day' , [2 3] , 'Horizon' , [3:13],...
+stats = se2_SigTest(Dall , 'MT' , 'seqNumb' , [0] , 'Day' , [4:5] , 'Horizon' , [3:13],...
     'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
-    'PoolHorizons' , [],'ipiOfInterest' , [] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
+    'PoolHorizons' , [6:13],'ipiOfInterest' , [] , 'poolIPIs' , 0 , 'subjnum' , [10]);
 
 %% significance test on RTs
 stats = se2_SigTest(Dall , 'RT' , 'seqNumb' , [0] , 'Day' , [1 4 5] , 'Horizon' , [1:13],...
@@ -100,9 +99,9 @@ stats = se2_SigTest(Dall , 'IPI' , 'seqNumb' , [0] , 'Day' , [1 :5] , 'Horizon' 
     'PoolHorizons' , [],'ipiOfInterest' , {[1] [2] [3] [4] [5] [6] [7] [8] [9] [10] [11] [12] [13]} , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
 
 %%
-stats = se2_SigTest(Dall , 'IPI' , 'seqNumb' , [0] , 'Day' , [5] , 'Horizon' , [2:13],...
-    'PoolDays' , 0,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
-    'PoolHorizons' , [7:13],'ipiOfInterest' , [0] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
+stats = se2_SigTest(Dall , 'IPI' , 'seqNumb' , [0:1] , 'Day' , [1:5] , 'Horizon' , [1:13],...
+    'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
+    'PoolHorizons' , [],'ipiOfInterest' , [0:2] , 'poolIPIs' , 0, 'subjnum' , [1:13]);
 %%
 
 %% significance test on IPIs
@@ -110,25 +109,25 @@ stats = se2_SigTest(Dall , 'IPI' , 'seqNumb' , [0] , 'Day' , [1] , 'Horizon' , [
     'PoolDays' , 0,'whatIPI','ipiOfInterestToSS','PoolSequences' , 0 ,...
     'PoolHorizons' , [],'ipiOfInterest' , [0] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
 %% single subject horizon significance test
-stats = se2_SigTest(Dall , 'PerSubjMTHorz' , 'seqNumb' , [0:2] , 'Day' , [1:3] , 'Horizon' , [1:13],...
+stats = se2_SigTest(Dall , 'PerSubjMTHorz' , 'seqNumb' , [0:2] , 'Day' , [1:5] , 'Horizon' , [1:13],...
     'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
     'PoolHorizons' , [7:13],'ipiOfInterest' , [0 2] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
 
 %% significance test for percent change in MTs (Random Structured)
-stats = se2_SigTest(Dall , 'PercentseqType' , 'seqNumb' , [0] , 'Day' , [1:5] , 'Horizon' , [7:13],...
-    'PoolDays' , 0,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
-    'PoolHorizons' , [6:13],'ipiOfInterest' , [0 : 2] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
+stats = se2_SigTest(Dall , 'PercentseqType' , 'seqNumb' , [0:1] , 'Day' , [1:5] , 'Horizon' , [1:13],...
+    'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
+    'PoolHorizons' , [],'ipiOfInterest' , [] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
 
 
 %% significance test for percent change in IPIs (random within between)
-stats = se2_SigTest(Dall , 'PercentIPItype' , 'seqNumb' , [0:2] , 'Day' , [5] , 'Horizon' , [2,7:13],...
-    'PoolDays' , 0,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
-    'PoolHorizons' , [7:13],'ipiOfInterest' , [0] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
+stats = se2_SigTest(Dall , 'PercentIPItype' , 'seqNumb' , [0:2] , 'Day' , [1: 5] , 'Horizon' , [1:13],...
+    'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
+    'PoolHorizons' , [],'ipiOfInterest' , [0:2] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
 
 %% significance test for percent change within sequence tpe acoss dayz
-stats = se2_SigTest(Dall , 'PercentMTwithinseqType' , 'seqNumb' , [0] , 'Day' , [1 2 3] , 'Horizon' , [3:13],...
+stats = se2_SigTest(Dall , 'PercentMTwithinseqType' , 'seqNumb' , [0:1] , 'Day' , [4 5] , 'Horizon' , [1:13],...
     'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
-    'PoolHorizons' , [],'ipiOfInterest' , [0] , 'poolIPIs' , 0 , 'subjnum' , [1:15]);
+    'PoolHorizons' , [],'ipiOfInterest' , [0] , 'poolIPIs' , 0 , 'subjnum' , [1:13]);
 %% Saccade Frequency
 stats = se2_SigTest(Dall , 'Eye_seq_sacPerSec' , 'seqNumb' , [0:2] , 'Day' , [1:5] , 'Horizon' , [1:13],...
     'PoolDays' , 1,'whatIPI','WithBetRand','PoolSequences' , 0 ,...
